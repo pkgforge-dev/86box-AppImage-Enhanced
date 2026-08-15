85e5e536390f079e097405c765059e056d28911e
Link4Electronics <link4electronics@gmail.com>
2026-08-15 05:38:44 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,9 +18,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
 # If the application needs to be manually built that has to be done down here
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
     make-aur-package 86box-roms-git
