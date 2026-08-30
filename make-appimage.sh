d6ad2933c11db63eec0d0727b9cacb2d6fe6c49d
Link4Electronics <link4electronics@gmail.com>
2026-08-30 06:13:05 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,8 +10,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/256x256/apps/net.86box.86Box.png
 export DESKTOP=/usr/share/applications/net.86box.86Box.desktop
 export STARTUPWMCLASS=net.86box.86Box
-export DEPLOY_QT=1
-export QT_DIR=qt6
 
 # Deploy dependencies
 quick-sharun /usr/bin/86Box
@@ -21,4 +19,4 @@ quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
